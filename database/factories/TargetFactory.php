19edcfb6caca79748a27b92008efd09e712f7aec
minyeoungKim <to__get___her@naver.com>
2019-04-15 15:17:11 +0900

    Update TargetFactory.php

--- database/factories/TargetFactory.php
+++ database/factories/TargetFactory.php
@@ -3,16 +3,14 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Models\Surveies\Target::class, function (Faker $faker) {
-    $job_id_min = App\Models\Users\Job::min('id');
-    $job_id_max = App\Models\Users\Job::max('id');
-   
+
+    $age = array(
+        10,
+        20,
+    );
     
     return [
-        
-        'start_age' => 10,
-        'end_age' => 49,
-        'gender' => rand(1,2),
-        'job_id' => $faker->numberBetween($job_id_min,$job_id_max),
-        
+        'age' => 10,
+        'gender' => rand(0,3),
     ];
 });
