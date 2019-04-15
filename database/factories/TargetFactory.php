cf2221ae27df6852fa4cb3639dc16a378737f532
minyeoungKim <to__get___her@naver.com>
2019-04-15 19:30:44 +0900

    Update TargetFactory.php

--- database/factories/TargetFactory.php
+++ database/factories/TargetFactory.php
@@ -4,13 +4,10 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Models\Surveies\Target::class, function (Faker $faker) {
 
-    $age = array(
-        10,
-        20,
-    );
+    $age = array( 10, 20 );
     
     return [
-        'age' => 10,
+        'age' => $age,
         'gender' => rand(0,3),
     ];
 });
