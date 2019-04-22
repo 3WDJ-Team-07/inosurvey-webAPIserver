04f4fff994f97a6665bde2553caaea79efb104c9
minyeoungKim <to__get___her@naver.com>
2019-04-22 17:50:25 +0900

    Update ModelScopes.php

--- app/Traits/ModelScopes.php
+++ app/Traits/ModelScopes.php
@@ -5,18 +5,21 @@ namespace App\Traits;
 trait ModelScopes{
 
     public function scopeInsertMsgs($query, Array $msgs){
-        $query->insert($msgs);
-        //return $this->donationModel->create(request()->all());
+        return $query->insert($msgs);
     }
 
-    public function scopeGetMsg($query,$id)
-    {
+    public function scopeGetMsg($query,$id){
         return $query->where('id', $id)->first();
     }
 
     //최근 생성된 컬럼 선택
     public function scopeGetLatest($query, $created_at){
-        return $query->latest($created_at)->first();
+        //return $query->latest($created_at)->get();
+        return $query->select('*')->orderBy($created_at,'DESC')->first();
+    }
+
+    public function scopeUpdateMsg($query, $id, $column, $value){
+        return $query->where('id', $id )->update([$column => $value]);
     }
 }
 ?>
