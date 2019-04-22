2a74afa0987b2f82a562085ea4b1df90c6c795b0
minyeoungKim <to__get___her@naver.com>
2019-04-22 17:50:03 +0900

    Update JobTarget.php

--- app/Models/Surveies/JobTarget.php
+++ app/Models/Surveies/JobTarget.php
@@ -3,9 +3,12 @@
 namespace App\Models\Surveies;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Traits\ModelScopes;
 
 class JobTarget extends Model
 {
+    use ModelScopes;
+    
     public $timestamps = false;
     protected $fillable = ['job_id','target_id',];
     protected $table = 'job_target';
