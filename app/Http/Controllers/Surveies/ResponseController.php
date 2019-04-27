52552f19cbb8490d5d989d4b5e7bf7c3e4f74283
パク・ボグン <33335769+pyc2238@users.noreply.github.com>
2019-04-27 21:32:57 +0900

    add response function

--- app/Http/Controllers/Surveies/ResponseController.php
+++ app/Http/Controllers/Surveies/ResponseController.php
@@ -1,20 +1,11 @@
 <?php
 
 namespace App\Http\Controllers\Surveies;
-
-use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
-
-use App\Models\Surveies\Form;
-use App\Models\Surveies\Question;
-use App\Models\Surveies\QuestionItem;
-use App\Models\Surveies\QuestionBank;
-use App\Models\Users\User;
 /**
  * 클래스명:                      ResponseController
  * @package                      App\Http\Controllers\Surveies
  * 클래스 설명:                   설문조사 응답 작업을 하는 컨트롤러
- * 만든이:                        3-WDJ 7조 ナナイロトリ 1701037 김민영
+ * 만든이:                        3-WDJ 7조 ナナイロトリ 1701037 김민영 1501107 박보근
  * 만든날:                        2019년 4월 10일
  *
  * 함수 목록
@@ -23,6 +14,17 @@ use App\Models\Users\User;
  * selectQuestionItem(파일)     파일 업로드
  * 
  */
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+use App\Models\Surveies\Form;
+use App\Models\Surveies\Question;
+use App\Models\Surveies\QuestionItem;
+use App\Models\Surveies\ItemResponse;
+use App\Models\Surveies\SurveyUser;
+use App\Models\Surveies\Response;
+use App\Models\Users\User;
+
 class ResponseController extends Controller
 { 
    
@@ -40,7 +42,7 @@ class ResponseController extends Controller
 
     //응답 내용 저장
     public function create(Request $request){
-        
+        return $request;
     }
     
     //설문 리스트 
