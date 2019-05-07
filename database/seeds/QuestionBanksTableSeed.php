fb655e7b9117b300706bf9bf0c6fe9c43406f39c
パク・ボグン <33335769+pyc2238@users.noreply.github.com>
2019-05-07 17:31:19 +0900

    modify questionBank seeder

--- database/seeds/QuestionBanksTableSeed.php
+++ database/seeds/QuestionBanksTableSeed.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Models\Surveies\QuestionBank;
 
 class QuestionBanksTableSeed extends Seeder
 {
@@ -11,6 +12,27 @@ class QuestionBanksTableSeed extends Seeder
      */
     public function run()
     {
-        factory(App\Models\Surveies\QuestionBank::class,10)->create();
+        // factory(App\Models\Surveies\QuestionBank::class,10)->create();
+    
+        $data = array(
+            $questions = array(
+                'survey_title' => "지난 12개월 동안 의료와 관계된 질문으로 정상 진료 시간 후에 ()에게 전화하셨습니까?",
+                    'values' =>array( 
+                        '귀하의 의료 제공자',
+                        '직접입력',
+                        ),
+                    'items' =>array(
+                        array('value' => '귀하의 의료 제공자'),
+                        array('value' => '귀하의 의료 제공자'),
+                        array('value' => '귀하의 의료 제공자'),
+                        array('value' => '귀하의 의료 제공자'),
+                        array('value' => '귀하의 의료 제공자'),
+                        ),
+                    ),
+            );
+
+            foreach($data as $rs){ 
+                QuestionBank::insert($rs);
+             } 
     }
 }
