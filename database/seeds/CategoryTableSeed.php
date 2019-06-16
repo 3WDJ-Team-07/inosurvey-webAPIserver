da7b0aed5bc8e4c8dd401a1923c7a48c180c7b45
パク・ボグン <33335769+pyc2238@users.noreply.github.com>
2019-06-16 15:18:40 +0900

    last commit

--- database/seeds/CategoryTableSeed.php
+++ database/seeds/CategoryTableSeed.php
@@ -16,11 +16,9 @@ class CategoryTableSeed extends Seeder
             array('name' => '아동 / 청소년'),
             array('name' => '어르신'),
             array('name' => '장애인'),
-            // array('name' => '다문화'),
             array('name' => '지구촌'),
             array('name' => '가족 / 여성'),
             array('name' => '시민사회'),
-            // array('name' => '동물'),
             array('name' => '동식물'),
             array('name' => '기타'),
             
