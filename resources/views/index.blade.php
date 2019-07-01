fd80b3f5a3d8f601ee5efa1c3a50f18e8c2a4ad4
パク・ボグン <33335769+pyc2238@users.noreply.github.com>
2019-07-01 09:13:31 +0900

    commit last

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,25 +7,22 @@
     <meta name=viewport content="width=device-width,initial-scale=1">
     <link rel=icon href=/favicon.ico> <title>INOSURVEY</title>
     <link href="https://fonts.googleapis.com/css?family=Noto+Sans+KR|Material+Icons" rel=stylesheet>
-    <link rel=stylesheet type=text/css href=https://cdn.rawgit.com/moonspam/NanumSquare/master/nanumsquare.css>
-     <link rel=stylesheet href=https://use.fontawesome.com/releases/v5.7.1/css/all.css
-           integrity=sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr crossorigin=anonymous>
+    <link rel=stylesheet type=text/css href=https://cdn.rawgit.com/moonspam/NanumSquare/master/nanumsquare.css> 
+    <link rel=stylesheet href=https://use.fontawesome.com/releases/v5.7.1/css/all.css
+        integrity=sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr crossorigin=anonymous>
     <link href=https://use.fontawesome.com/releases/v5.0.13/css/all.css rel=stylesheet>
-    <link href=/css/vuetify.min.css rel=stylesheet>
-    <link href=/css/app.6e650852.css rel=preload as=style>
+    <link href=vuetify/dist/vuetify.min.css rel=stylesheet>
+    <link href=/css/app.ddcec34b.css rel=preload as=style>
     <link href=/css/chunk-vendors.1fa394ca.css rel=preload as=style>
-    <link href=/js/app.8752c25d.js rel=preload as=script>
+    <link href=/js/app.881724c6.js rel=preload as=script>
     <link href=/js/chunk-vendors.18a726a4.js rel=preload as=script>
     <link href=/css/chunk-vendors.1fa394ca.css rel=stylesheet>
-    <link href=/css/app.6e650852.css rel=stylesheet>
+    <link href=/css/app.ddcec34b.css rel=stylesheet>
 </head>
 
 <body>
     <noscript>
         <strong>We're sorry but inosurvey doesn't work properly without JavaScript enabled. Please enable it tocontinue.</strong>
-    </noscript>
+        </noscript>
     <div id=app></div>
-    <script src=/js/chunk-vendors.18a726a4.js></script>
-    <script src=/js/app.8752c25d.js></script>
-</body>
-</html>
+    <script src=/js/chunk-vendors.18a726a4.js> </script> <script src=/js/app.881724c6.js> </script> </body> </html>
